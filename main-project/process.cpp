1723c174ce301d875cf1641f3da6558fd9639606
isavelyev76 <etodlajava@gmail.com>
2023-12-26 19:38:57 +0300

    Реализована функция обработки данных

--- main-project/process.cpp
+++ main-project/process.cpp
@@ -1,7 +1,12 @@
 #include "processing.h"
+#include <iostream>
 
-double process(Product* array[], int size)
-{
-
-    return 0;
+double calculateTotalCost(const Product* products, int size, const char* category) {
+    double totalCost = 0.0;
+    for (int i = 0; i < size; i++) {
+        if (strcmp(products[i].category, category) == 0) {
+            totalCost += products[i].price * products[i].quantity;
+        }
+    }
+    return totalCost;
 }
